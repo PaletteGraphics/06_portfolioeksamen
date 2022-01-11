a6ef0c6a82e07293968332ae7c43c7c2d82daa48
Frederik Oerregaard <fred04q2@stud.kea.dk>
2022-01-11 12:29:00 +0100

    PortAlmostDone

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const body = document.querySelector("body");
 // Lav en vaiabel, der refererer til ".mode"
 const mode = document.querySelector(".mode");
 // 1. Lav en funktion, der hedder darkMode
-// 2. Toggle en klasse, der hedder "dark" vha. classList
-// eller
-// 1. Lav en funktion, der hedder darkMode
 // 2. Ændr teksten på toggle-knappen og tilføj eller fjern klassen "dark" vha. en if/else sætning
 function darkMode() {
   if (body.classList.contains("dark")) {
@@ -16,27 +13,5 @@ function darkMode() {
     body.classList.add("dark");
   }
 }
-// Lav et klik-event på toggle, der sætter funktionen darkMode i gang
+// // Lav et klik-event på toggle, der sætter funktionen darkMode i gang
 mode.addEventListener("click", darkMode);
-
-//Tag fat i knap
-mybutton = document.getElementById("myBtn");
-
-// Når brugeren scroller 20px fra toppen vises pilen
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
